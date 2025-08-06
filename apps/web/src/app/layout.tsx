b0c6a187d88a1346c1472335f19f141a58af8a8a
beambeambeam <supawitmarayat@gmail.com>
2025-08-06 11:17:43 +0700

    feat: add ThemeProvider and integrate next-themes for theme management

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import { ThemeProvider } from "@/provider/themes";
+
+import "./global.css";
+
+export const metadata = {
+  title: "The Codex",
+  description: "Your personal Librarian that handle every file for you.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-background h-full min-h-screen w-full antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          themes={["light", "dark"]}
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
