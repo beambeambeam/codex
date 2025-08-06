524d11e3efa71803767b0d5faad2c78d1eac3c9d
beambeambeam <supawitmarayat@gmail.com>
2025-08-06 13:49:55 +0700

    feat: add logo and langing

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,20 @@
-import { ToggleThemeButton } from "@/components/ui/toggle-theme";
-import { env } from "@/env";
+import Link from "next/link";
 
-function LandingPage() {
+import { Button } from "@/components/ui/button";
+import { Logo } from "@/components/ui/logo";
+
+function Page() {
   return (
-    <div>
-      <div>API URL: {env.NEXT_PUBLIC_API_URL}</div>
-      <div className="flex w-fit">
-        <ToggleThemeButton />
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+      <Logo size={400} />
+      <div className="flex flex-col gap-2 text-6xl font-bold">
+        Make every data, Askable.
       </div>
-      กาาร
+      <Link href="/sign-in">
+        <Button variant="outline">Let&apos;s get start!</Button>
+      </Link>
     </div>
   );
 }
-export default LandingPage;
+
+export default Page;
