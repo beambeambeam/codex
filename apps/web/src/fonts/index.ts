8501f1cd83206bf6661b01a15457e71dfbc7d9cf
beambeambeam <supawitmarayat@gmail.com>
2025-08-06 11:56:59 +0700

    feat: add additional weights for LineSeedSans font and update Sarabun weights

--- apps/web/src/fonts/index.ts
+++ apps/web/src/fonts/index.ts
@@ -15,13 +15,21 @@ export const LineSeedSans = localFont({
       path: "./line/LINESeedSansTH_W_Bd.woff",
       weight: "700",
     },
+    {
+      path: "./line/LINESeedSansTH_W_XBd.woff",
+      weight: "800",
+    },
+    {
+      path: "./line/LINESeedSansTH_W_He.woff",
+      weight: "900",
+    },
   ],
   display: "swap",
   variable: "--font-line-seed-sans",
 });
 
 export const Sarabun = SarabunGoogle({
-  weight: "400",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
   display: "swap",
   variable: "--font-sarabun",
   subsets: ["latin", "thai"],
