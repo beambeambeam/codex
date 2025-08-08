02335a306463d15c28471ed11a607b1275539008
beambeambeam <supawitmarayat@gmail.com>
2025-08-08 09:20:25 +0700

    refactor: change FormProps to default export and remove unused properties

--- apps/web/src/types/form.ts
+++ apps/web/src/types/form.ts
@@ -1,8 +1,5 @@
-export interface FormProps<SchemaType> {
+export default interface FormProps<SchemaType> {
   defaultValues?: SchemaType;
   onSubmit?: (values: SchemaType) => void;
-  isPending?: boolean;
-  isSuccess?: boolean;
-  isError?: boolean;
   disabled?: boolean;
 }
