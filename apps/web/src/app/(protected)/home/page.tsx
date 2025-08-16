b39fc8f91dc97799e5ae25033b2557a153f91134
beambeambeam <supawitmarayat@gmail.com>
2025-08-16 15:25:33 +0700

    refactor: enhance HomePage layout and user greeting

--- apps/web/src/app/(protected)/home/page.tsx
+++ apps/web/src/app/(protected)/home/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import Settings from "@/components/settings";
-import SignOutButton from "@/components/sign-out";
 import { useUser } from "@/store/user";
 
 function HomePage() {
   const { user } = useUser();
 
   return (
-    <div>
-      <Settings />
-      {user?.display} <SignOutButton />
+    <div className="flex h-screen w-full flex-col space-y-1 p-4">
+      <div className="flex w-full justify-between">
+        <div className="text-xl font-bold">Welcome back, {user?.display}.</div>
+        <Settings />
+      </div>
+      <div className="h-full rounded-lg border-2"></div>
     </div>
   );
 }
