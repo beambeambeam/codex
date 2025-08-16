679195455eff694d4aa92737e416356b7cfe4908
beambeambeam <supawitmarayat@gmail.com>
2025-08-16 20:47:37 +0700

    feat: implement Book component and integrate CollectionBooks into HomePage

--- apps/web/src/app/(protected)/home/page.tsx
+++ apps/web/src/app/(protected)/home/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import { LibraryBigIcon, StarIcon } from "lucide-react";
+import { parseAsString, useQueryState } from "nuqs";
+
+import CollectionBooks from "@/app/(protected)/home/_components/book";
 import HomeCanvas from "@/app/(protected)/home/_components/canvas";
 import CreateCollectionDialog from "@/app/(protected)/home/_components/create";
 import HomeSearch from "@/app/(protected)/home/_components/search";
 import Settings from "@/components/settings";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useUser } from "@/store/user";
 
 function HomePage() {
+  const [tab, setTab] = useQueryState("tab", parseAsString.withDefault("g"));
+
+  const onTabChange = (value: string) => setTab(value);
   const { user } = useUser();
 
   return (
@@ -19,8 +27,37 @@ function HomePage() {
           <Settings />
         </div>
       </div>
-      <div className="h-full rounded-lg border-2">
-        <HomeCanvas display={user?.display ?? ""} />
+      <div className="relative h-full overflow-hidden rounded-lg border-2">
+        <Tabs
+          defaultValue="g"
+          className="absolute z-50 w-full lg:left-4 lg:top-4"
+          value={tab}
+          orientation="vertical"
+          onValueChange={onTabChange}
+        >
+          <TabsList className="h-full flex-col">
+            <TabsTrigger value="g">
+              <StarIcon />
+              Webs
+            </TabsTrigger>
+            <TabsTrigger value="b">
+              <LibraryBigIcon />
+              Books
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <div
+          style={{ display: tab === "g" ? "block" : "none" }}
+          className="h-full w-full"
+        >
+          <HomeCanvas display={user?.display ?? ""} />
+        </div>
+        <div
+          className="h-full w-full"
+          style={{ display: tab === "b" ? "block" : "none" }}
+        >
+          <CollectionBooks />
+        </div>
       </div>
     </div>
   );
