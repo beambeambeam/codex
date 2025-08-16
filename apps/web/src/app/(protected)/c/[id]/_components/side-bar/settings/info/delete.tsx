e62db52fed80d18ad8c56388a69941c4a5dfb61f
beambeambeam <supawitmarayat@gmail.com>
2025-08-17 01:07:14 +0700

    feat: add CollectionDelete component for collection deletion functionality

---
+++ apps/web/src/app/(protected)/c/[id]/_components/side-bar/settings/info/delete.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@/components/ui/button";
+
+function CollectionDelete() {
+  return <Button variant="destructive">Delete This Collection</Button>;
+}
+export default CollectionDelete;
