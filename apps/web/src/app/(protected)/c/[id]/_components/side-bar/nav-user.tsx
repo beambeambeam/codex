0c6ae4edc8b9b6de789434bf2b8daffc8b0833b4
beambeambeam <supawitmarayat@gmail.com>
2025-08-16 23:04:04 +0700

    feat: simplify NavUser component by removing dropdown and enhancing layout

--- apps/web/src/app/(protected)/c/[id]/_components/side-bar/nav-user.tsx
+++ apps/web/src/app/(protected)/c/[id]/_components/side-bar/nav-user.tsx
@@ -1,17 +1,8 @@
 "use client";
 
-import { ChevronsUpDown } from "lucide-react";
-
+import Settings from "@/components/settings";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar";
+import { SidebarMenu, SidebarMenuItem } from "@/components/ui/sidebar";
 import { useUser } from "@/store/user";
 
 export function NavUser() {
@@ -22,24 +13,18 @@ export function NavUser() {
   }
 
   return (
-    <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton size="lg">
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarFallback className="rounded-lg">
-                  {user.display[0]}
-                </AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.display}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
-              <ChevronsUpDown className="ml-auto size-4" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-        </DropdownMenu>
+    <SidebarMenu className="w-full">
+      <SidebarMenuItem className="flex w-full justify-between gap-2">
+        <Avatar className="h-8 w-8 rounded-lg">
+          <AvatarFallback className="rounded-lg">
+            {user.display[0]}
+          </AvatarFallback>
+        </Avatar>
+        <div className="grid flex-1 text-left text-sm leading-tight">
+          <span className="truncate font-medium">{user.display}</span>
+          <span className="truncate text-xs">{user.email}</span>
+        </div>
+        <Settings />
       </SidebarMenuItem>
     </SidebarMenu>
   );
