f842e65d978d4b8b21f464a99ae9242eb044b531
beambeambeam <supawitmarayat@gmail.com>
2025-08-16 23:27:51 +0700

    feat: add Summary section to CollectionInfo component and adjust layout

--- apps/web/src/app/(protected)/c/[id]/_components/side-bar/info.tsx
+++ apps/web/src/app/(protected)/c/[id]/_components/side-bar/info.tsx
@@ -1,4 +1,4 @@
-import { BadgeAlertIcon } from "lucide-react";
+import { BadgeAlertIcon, NotebookPenIcon } from "lucide-react";
 
 import { useCollectionState } from "@/app/(protected)/c/[id]/_components/context";
 import {
@@ -21,10 +21,23 @@ function CollectionInfo() {
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           <SidebarMenuItem className="px-2">
-            <div className="rounded-lg border-2 p-1 px-3 text-sm">
+            <div className="text-wrap text-xs">
               {collection.description
                 ? collection.description
-                : "No description yet"}
+                : "No description yet."}
+            </div>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroupContent>
+      <SidebarGroupLabel className="mt-3 flex items-center gap-0.5">
+        <NotebookPenIcon />
+        Summary
+      </SidebarGroupLabel>
+      <SidebarGroupContent className="flex flex-col gap-2">
+        <SidebarMenu>
+          <SidebarMenuItem className="px-2">
+            <div className="text-wrap text-xs">
+              {collection.summary ? collection.summary : "No summary yet."}
             </div>
           </SidebarMenuItem>
         </SidebarMenu>
