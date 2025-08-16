5bc300c981e7d2b903e0bc3c3f70f3bca873a1fe
beambeambeam <supawitmarayat@gmail.com>
2025-08-17 00:19:14 +0700

    feat: update header styling in CollectionPage for improved layout consistency

--- apps/web/src/app/(protected)/c/[id]/page.tsx
+++ apps/web/src/app/(protected)/c/[id]/page.tsx
@@ -10,13 +10,13 @@ function CollectionPage() {
     <SidebarProvider>
       <CollectionSidebar />
       <SidebarInset>
-        <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+        <header className="group-has-data-[collapsible=offcanvas]/sidebar-wrapper:h-12 h-16.5 flex shrink-0 items-center gap-2 transition-[width,height] ease-linear">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
           </div>
         </header>
 
-        <section className="flex h-full gap-2 px-4 pt-2">d</section>
+        <section className="flex h-full gap-2 border-t-2 px-4 pt-2"></section>
       </SidebarInset>
     </SidebarProvider>
   );
