9e64d8f6b0d1d479098687178ab7017c54b6cbc5
beambeambeam <supawitmarayat@gmail.com>
2025-08-16 18:15:15 +0700

    feat: enhance CollectionNode with Badge component for improved description and summary display

--- apps/web/src/app/(protected)/home/_components/canvas/node/collection-node.tsx
+++ apps/web/src/app/(protected)/home/_components/canvas/node/collection-node.tsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 import { Handle, Position } from "@xyflow/react";
-import { Rocket } from "lucide-react";
+import { AlbumIcon, BadgeInfoIcon } from "lucide-react";
 
 import {
   BaseNode,
@@ -8,6 +8,7 @@ import {
   BaseNodeHeader,
   BaseNodeHeaderTitle,
 } from "@/components/base-node";
+import { Badge } from "@/components/ui/badge";
 
 interface CollectionNodeProps {
   data: {
@@ -27,22 +28,34 @@ export const CollectionNode = memo((props: CollectionNodeProps) => {
         className="invisible !bottom-auto !left-1/2 !right-auto !top-1/2 !-translate-x-1/2 !-translate-y-1/2"
       />
       <BaseNodeHeader className="border-b">
-        <Rocket className="size-4" />
+        <AlbumIcon className="size-4" />
         <BaseNodeHeaderTitle>
           {props.data.title ? props.data.title : "Untitled Collection"}
         </BaseNodeHeaderTitle>
       </BaseNodeHeader>
       <BaseNodeContent>
-        <div>
-          <p className="text-xs">
-            {props.data.description
-              ? props.data.description
-              : "No description available for this collection."}
-          </p>
+        <div className="flex flex-col gap-y-6">
+          <div className="flex flex-col gap-y-2">
+            <Badge variant="secondary">
+              <BadgeInfoIcon className="size-4" />
+              description
+            </Badge>
+            {props.data.description ? (
+              <p className="text-xs">{props.data.description}</p>
+            ) : (
+              <p className="text-xs">
+                No description available for this collection.
+              </p>
+            )}
+          </div>
           {props.data.summary && (
-            <p className="text-muted-foreground mt-2 text-xs">
-              {props.data.summary}
-            </p>
+            <div className="flex flex-col gap-y-2">
+              <Badge variant="secondary">
+                <BadgeInfoIcon className="size-4" />
+                summary
+              </Badge>
+              <p className="text-xs">{props.data.summary}</p>
+            </div>
           )}
         </div>
       </BaseNodeContent>
