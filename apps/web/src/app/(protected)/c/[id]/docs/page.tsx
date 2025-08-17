2bae4b5473ed13de2bde65794cfd68d9e3022c76
beambeambeam <supawitmarayat@gmail.com>
2025-08-18 01:06:01 +0700

    feat: add CollectionBreadcrums component and integrate Select UI component

--- apps/web/src/app/(protected)/c/[id]/docs/page.tsx
+++ apps/web/src/app/(protected)/c/[id]/docs/page.tsx
@@ -1,3 +1,4 @@
+import CollectionBreadcrums from "@/app/(protected)/c/[id]/_components/breadcrums";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 
 function DocsPage() {
@@ -6,6 +7,7 @@ function DocsPage() {
       <header className="group-has-data-[collapsible=offcanvas]/sidebar-wrapper:h-12 h-16.5 flex shrink-0 items-center gap-2 transition-[width,height] ease-linear">
         <div className="flex items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
+          <CollectionBreadcrums />
         </div>
       </header>
       <section className="flex h-full gap-2 border-t-2 px-4 pt-2"></section>
