290682f8eadd9259685994e4f0bd1325d4a30493
beambeambeam <supawitmarayat@gmail.com>
2025-08-18 01:07:11 +0700

    refactor: remove unused header and sidebar components from DocsPage and CollectionPage

--- apps/web/src/app/(protected)/c/[id]/page.tsx
+++ apps/web/src/app/(protected)/c/[id]/page.tsx
@@ -1,15 +1,4 @@
-import { SidebarTrigger } from "@/components/ui/sidebar";
-
 function CollectionPage() {
-  return (
-    <>
-      <header className="group-has-data-[collapsible=offcanvas]/sidebar-wrapper:h-12 h-16.5 flex shrink-0 items-center gap-2 transition-[width,height] ease-linear">
-        <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1" />
-        </div>
-      </header>
-      <section className="flex h-full gap-2 border-t-2 px-4 pt-2"></section>
-    </>
-  );
+  return <section className="flex h-full gap-2 border-t-2 px-4 pt-2"></section>;
 }
 export default CollectionPage;
