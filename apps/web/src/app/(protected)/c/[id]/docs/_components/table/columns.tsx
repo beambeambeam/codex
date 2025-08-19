372c828afe13b53f6bd3a6d534f916e3849b7dc2
beambeambeam <supawitmarayat@gmail.com>
2025-08-19 14:32:20 +0700

    feat: enhance document table columns with improved cell rendering and new upload information

--- apps/web/src/app/(protected)/c/[id]/docs/_components/table/columns.tsx
+++ apps/web/src/app/(protected)/c/[id]/docs/_components/table/columns.tsx
@@ -3,6 +3,8 @@ import { Text } from "lucide-react";
 
 import DocumentAcions from "@/app/(protected)/c/[id]/docs/_components/table/actions";
 import { DataTableColumnHeader } from "@/components/data-table/header";
+import { Pill, PillAvatar } from "@/components/ui/pill";
+import { RelativeTimeCard } from "@/components/ui/relative-time-card";
 import { components } from "@/lib/api/path";
 
 export type DocumentsType = components["schemas"]["DocumentResponse"];
@@ -10,7 +12,7 @@ export type DocumentsType = components["schemas"]["DocumentResponse"];
 const columnHelper = createColumnHelper<DocumentsType>();
 
 export const documentColumns = [
-  columnHelper.accessor("id", {
+  columnHelper.accessor("file.name", {
     header: ({ column }) => {
       return <DataTableColumnHeader column={column} title="File Name" />;
     },
@@ -27,10 +29,13 @@ export const documentColumns = [
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Title" />
     ),
-    cell: (info) => info.getValue() ?? "",
+    cell: (info) =>
+      info.getValue() ?? (
+        <p className="text-muted-foreground">Untitled Title</p>
+      ),
     meta: {
       label: "Title",
-      placeholder: "Search titles...",
+      placeholder: "Filter by title...",
       variant: "text",
       icon: Text,
     },
@@ -40,10 +45,51 @@ export const documentColumns = [
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Description" />
     ),
-    cell: (info) => info.getValue() ?? "",
+    cell: (info) =>
+      info.getValue() ?? (
+        <p className="text-muted-foreground">No description provided</p>
+      ),
     meta: {
       label: "Description",
-      placeholder: "Search descriptions...",
+      placeholder: "Filter by description...",
+      variant: "text",
+      icon: Text,
+    },
+    enableColumnFilter: true,
+  }),
+  columnHelper.accessor("file.upload_by", {
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Uploaded By" />
+    ),
+    cell: (info) =>
+      info.getValue() ? (
+        <Pill>
+          <PillAvatar
+            fallback={info.row.original.user?.display[0]}
+            src=""
+            className="border"
+          />
+          {info.row.original.user?.display}
+        </Pill>
+      ) : (
+        <p className="text-muted-foreground">Unknown</p>
+      ),
+    meta: {
+      label: "Uploaded By",
+      placeholder: "Search uploader...",
+      variant: "text",
+      icon: Text,
+    },
+    enableColumnFilter: true,
+  }),
+  columnHelper.accessor("file.upload_at", {
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Uploaded At" />
+    ),
+    cell: (info) => <RelativeTimeCard date={info.getValue() ?? ""} />,
+    meta: {
+      label: "Uploaded At",
+      placeholder: "Search upload dates...",
       variant: "text",
       icon: Text,
     },
